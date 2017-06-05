552b706b68a45537113b0e535b1442518aca3b8b
liyikun <liyikun@le.com>
2017-06-06 00:02:09 +0800

    upload example of reading list

---
+++ spring-boot-reading-list/src/main/java/readinglist/ReadingListApplication.java
@@ -0,0 +1,15 @@
+package readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liyikun on 2017/6/5.
+ */
+@SpringBootApplication
+public class ReadingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingListApplication.class, args);
+    }
+}
---
+++ spring-boot-reading-list/src/test/java/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,19 @@
+package readinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by liyikun on 2017/6/5.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ReadingListApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
